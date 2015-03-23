fa37426064665d8526653a7173f18acdc91b4111
Davydov Valeriy <em0va1@rambler.ru>
2015-03-23 13:26:39 +0200

    add metro checkbox

---
+++ CheckboxColumn.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace creators\metroui\grid;
+
+use Yii;
+use yii\helpers\Html;
+
+class CheckboxColumn extends \yii\grid\CheckboxColumn
+{
+    protected function renderHeaderCellContent()
+    {
+        $name = rtrim($this->name, '[]') . '_all';
+        $id = $this->grid->options['id'];
+        $options = json_encode([
+            'name' => $this->name,
+            'multiple' => $this->multiple,
+            'checkAll' => $name,
+        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        $this->grid->getView()->registerJs("jQuery('#$id').yiiGridView('setSelectionColumn', $options);");
+
+        if ($this->header !== null || !$this->multiple) {
+            return parent::renderHeaderCellContent();
+        } else {
+            return '<div class="input-control checkbox"><label>'.
+                    Html::checkBox($name, false, ['class' => 'select-on-check-all']).
+                    '<span class="check"></span></label></div>';
+        }
+    }
+    
+    protected function renderDataCellContent($model, $key, $index)
+    {
+        if ($this->checkboxOptions instanceof Closure) {
+            $options = call_user_func($this->checkboxOptions, $model, $key, $index, $this);
+        } else {
+            $options = $this->checkboxOptions;
+            if (!isset($options['value'])) {
+                $options['value'] = is_array($key) ? json_encode($key, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) : $key;
+            }
+        }
+
+        return '<div class="input-control checkbox"><label>'.
+                Html::checkbox($this->name, !empty($options['checked']), $options).
+            '<span class="check"></span></label></div>';
+    }
+}
